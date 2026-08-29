7da4d62b3908934e634150fb41d97ff015ba1ab4
Aj Cabasis <ronron110912@gmail.com>
2026-08-29 21:05:44 +0800

    Added popup element

---
+++ js/OpenSource.js
@@ -0,0 +1,21 @@
+const container = document.getElementById("container");
+const close  = document.getElementById("close");
+const popup = document.getElementById("popup");
+const accept = document.getElementById("green-btn-2");
+const shareSitepack = document.getElementById("share-sitepack");
+
+
+close.addEventListener('click', () => {
+    popup.style.display = 'none';
+    container.classList.remove('blurred');
+});
+
+accept.addEventListener('click', () => {
+    popup.style.display = 'none';
+    container.classList.remove('blurred');
+});
+
+shareSitepack.addEventListener('click', () => {
+    popup.style.display = 'block';
+    container.classList.add('blurred');
+});
